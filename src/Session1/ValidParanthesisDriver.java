a93cb16c3e37033300c511e7fbe8a8f7de78a7ea
Sriram Vellangallor Subramanian <sriramoman@gmail.com>
2018-08-20 23:20:52 -0700

    Changelog:     20. Valid Parentheses
    Signed-off-by: Sriram Vellangallor Subramanian <sriramoman@gmail.com>

--- src/Session1/ValidParanthesisDriver.java
+++ src/Session1/ValidParanthesisDriver.java
@@ -6,18 +6,15 @@ import java.util.Stack;
 public class ValidParanthesisDriver {
 
     public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
         for (char bkt : s.toCharArray()) {
-            if (bkt == '(' || bkt == '{' || bkt == '[')
-                stack.push(new Character(bkt));
-            else if (map.containsKey(bkt) && !stack.isEmpty() && stack.peek().compareTo(map.get(bkt)) == 0)
-                stack.pop();
-            else
-                return false;
+            if (bkt == '(' || bkt == '{' || bkt == '[') stack.push(new Character(bkt));
+            else if (map.containsKey(bkt) && !stack.isEmpty() && stack.peek().compareTo(map.get(bkt)) == 0) stack.pop();
+            else return false;
         }
         return stack.isEmpty();
     }
