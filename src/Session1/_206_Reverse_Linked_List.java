eca460ba124b6754743b5157b54a39db8909fb1f
Sriram Vellangallor Subramanian <sriramoman@gmail.com>
2018-08-26 23:40:09 -0700

    Changelog:     206. Reverse Linked List
    Signed-off-by: Sriram Vellangallor Subramanian <sriramoman@gmail.com>

---
+++ src/Session1/_206_Reverse_Linked_List.java
@@ -0,0 +1,28 @@
+package Session1;
+
+import java.util.Stack;
+
+public class _206_Reverse_Linked_List {
+    
+    public static void main(String[] args) {
+        //code
+    }
+
+    public ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode iter = head;
+        while (iter != null) {
+            stack.push(iter);
+            iter = iter.next;
+        }
+        ListNode dummyHead = new ListNode(0);
+        iter = dummyHead;
+        while (!stack.isEmpty()) {
+            ListNode node = stack.pop();
+            iter.next = node;
+            iter = iter.next;
+        }
+        iter.next = null;
+        return dummyHead.next;
+    }
+}
